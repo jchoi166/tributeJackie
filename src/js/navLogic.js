f2d80677e14c952a01cfd425df2b2c04cb38184e
Spike <jchoi166@gmail.com>
2020-12-20 15:18:32 -0800

    need to fix nav-scrolled

--- src/js/navLogic.js
+++ src/js/navLogic.js
@@ -3,8 +3,10 @@ console.log('its working')
 const nav = document.querySelector(".nav")
 const navScrolled = document.querySelector(".nav-scrolled")
 const pageWrapper = document.querySelector(".page-wrapper")
-
-
+const navClicked = document.querySelector(".nav-clicked")
+const navIconWrapper = document.querySelector(".nav__icon-wrapper")
+const navIcon = document.querySelector(".nav__icon")
+const body = document.getElementsByTagName("body")[0]
 
 const pageWrapperOptions = {
     root: null,
@@ -30,3 +32,44 @@ const pageWrapperObserver = new IntersectionObserver (function(entries, pageWrap
 
 pageWrapperObserver.observe(pageWrapper)
 
+const showNavMenu = () => {
+    navClicked.style.display = "flex";
+    /**
+  * Force a browser re-paint so the browser will realize the
+  * element is no longer `hidden` and allow transitions.
+  */
+    const reflow = navClicked.offsetHeight;
+    navClicked.style.opacity = 1;
+    navIcon.classList.add("nav__icon--active")
+}
+
+/** Storing display none in a variable so that we can freely add/remove it after transitionend */
+const listener = () => {
+    navClicked.style.display = "none";
+    navClicked.removeEventListener('transitionend', listener);
+};
+  
+const hideNavMenu = () => {
+    navClicked.addEventListener('transitionend', listener);
+    navClicked.style.opacity = 0;
+    navIcon.classList.remove("nav__icon--active")
+}
+
+navIconWrapper.addEventListener('click', () => {
+    // console.log("working!")
+    
+    if (navClicked.style.display == "none") {
+        // navClicked.style.display = "flex";
+        // navClicked.style.opacity = 1;
+        // navIcon.classList.add("nav__icon--active")
+        body.style.overflow = "hidden"
+        // console.log(body)
+        showNavMenu()
+      } else {
+        // navClicked.style.display = "none";
+        // navClicked.style.opacity = 0;
+        // navIcon.classList.remove("nav__icon--active")
+        body.style.overflow = "visible"
+        hideNavMenu()
+      }
+})
